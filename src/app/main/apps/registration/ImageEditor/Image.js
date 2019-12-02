4f5e302ee7ae0917edfc297ddfeb70ab072b874b
OFlag1014 <humblehorse1014@gmail.com>
2019-12-02 22:57:51 +0200

    fix image hook component

--- src/app/main/apps/registration/ImageEditor/Image.js
+++ src/app/main/apps/registration/ImageEditor/Image.js
@@ -10,10 +10,54 @@ import HomePage from './ImageEditor';
 import Modal from '@material-ui/core/Modal';
 import { makeStyles } from '@material-ui/core/styles';
 
-function Image(props)
-{
+const useStyles = makeStyles(theme => ({
+    paper: {
+        position: 'absolute',
+        width: 400,
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: theme.shadows[5],
+    },
+
+    photo: {
+        position: 'absolute',
+        left: '62%',
+        width: '120px',
+        height:'180px',
+        right: '0',
+        top: '15%',
+    },
+
+    backGround: {
+        width:'100%'
+    },
+
+    nameStyle: {
+        position: 'absolute',
+        top: '57%',
+        left: '24%',
+        color: 'midnightblue', 
+    },
+
+    companyNameStyle: {
+        position: 'absolute',
+        top: '64%',
+        left: '40%',
+    },
+
+    photoImg: {
+        width:'100%'
+    }
+}));
+
+function Image(props) {
+    
     const dispatch = useDispatch();
     const images = useSelector(({registerApp}) => registerApp.products.data);
+    const [modalStyle] = React.useState(getModalStyle);
+    const [open, setOpen] = React.useState(false);
+    
+    const classes = useStyles();
+    
     useEffect(() => {
         dispatch(Actions.getProducts());
     }, [dispatch]);
@@ -21,16 +65,14 @@ function Image(props)
     useEffect(() => {
         dispatch(Actions.setImage(images));
     }, [dispatch]);
+    
+    const { id } = props.match.params;
+    const image = images && images.filter((image) => {
+        return image.id === parseInt(id);
+    });
+    const name = image && image[0] && (image[0].firstName + ' ' + image[0].middleName + ' ' + image[0].lastName);
+    const imgSrc = image && image[0] && (`data:${image[0].mainPhotoContentType};base64, ${image[0].mainPhoto}`);
 
-    let id = props.match.params.id;
-    let image_url = images.filter((image) => {return image.id = id })[0].mainPhoto;
-    let image_type = images.filter((product) => {return product.id = id })[0].mainPhotoContentType;
-    const firstName = images.filter((product) => {return product.id = id })[0].firstName;
-    const middleName = images.filter((product) => {return product.id = id })[0].middleName;
-    const lastName = images.filter((product) => {return product.id = id })[0].lastName;
-    const companyName = images.filter((product) => {return product.id = id })[0].companyName;
-    const name = firstName+' '+middleName+' '+lastName;
-    const image = `data:${image_type};base64,${image_url}`;
     function getModalStyle() {
         const top = 50;
         const left = 50;
@@ -42,44 +84,7 @@ function Image(props)
         };
     }
       
-    const useStyles = makeStyles(theme => ({
-    paper: {
-        position: 'absolute',
-        width: 400,
-        backgroundColor: theme.palette.background.paper,
-        boxShadow: theme.shadows[5],
-    },
-    }));
-    const photo = {
-        position: 'absolute',
-        left: '62%',
-        width: '120px',
-        height:'180px',
-        right: '0',
-        top: '15%',
-    }
-    const background = {
-        width:'100%'
-    }
-    const name_style = {
-        position: 'absolute',
-        top: '57%',
-        left: '24%',
-        color: 'midnightblue',
-    }
-    const companyName_style = {
-        position: 'absolute',
-        top: '64%',
-        left: '40%',
-    }
-    const photo_img = {
-        width:'100%'
-    }
 
-    const classes = useStyles();
-    // getModalStyle is not a pure function, we roll the style only on the first render
-    const [modalStyle] = React.useState(getModalStyle);
-    const [open, setOpen] = React.useState(false);
     const handleOpen = () => {
         setOpen(true);
     };
@@ -93,29 +98,27 @@ function Image(props)
             <FusePageCarded
                 header = {
                     <div className="flex flex-1 w-full items-center justify-between">
-
                         <div className="flex items-center">
                             <FuseAnimate animation="transition.expandIn" delay={300}>
-                                <Typography className="normal-case flex items-center sm:mb-12" component={Link} role="button" to="/app/registeration/registeration-forms" color="inherit">
+                                <Typography className="normal-case flex items-center sm:mb-12" component={Link} role="button" to="/app/registration/registration-forms" color="inherit">
                                     <Icon className="text-32 mr-0 sm:mr-12">arrow_back</Icon>
                                 </Typography>
                             </FuseAnimate>
                             <FuseAnimate animation="transition.slideLeftIn" delay={300}>
-                                <Typography className="hidden sm:flex" variant="h6">Registeration</Typography>
+                                <Typography className="hidden sm:flex" variant="h6">Registration</Typography>
                             </FuseAnimate>
                         </div>
                         <FuseAnimate animation="transition.slideRightIn" delay={300}>
                             <Button onClick={handleOpen} className="whitespace-no-wrap" variant="contained">
                                 <span className="hidden sm:flex">Print Badge</span>
-                                {/* <span className="flex sm:hidden">New</span> */}
                             </Button>
                         </FuseAnimate>
                     </div>
                 }
                 content={
                      (
-                         <div>
-                            <HomePage image = {image} />
+                         <React.Fragment>
+                            <HomePage image = {imgSrc} />
                             <Modal
                                 aria-labelledby="print-modal-title"
                                 aria-describedby="print-modal-description"
@@ -123,16 +126,20 @@ function Image(props)
                                 onClose={handleClose}
                             >
                                 <div style={modalStyle}  className={classes.paper}>
-                                    <h1 style={name_style}>{name}</h1>
-                                    <h2 style={companyName_style}>{companyName}</h2>
-                                    <img src ='assets/images/background/background.png' style={background} alt={'port-0'}/>
-                                    <div style={photo}>
-                                        <img src = {image} style={photo_img} delay={100} alt={'port-1'}/>
-                                    </div>
+                                    {image && image[0] && (
+                                        <React.Fragment>
+                                            <h1 className={classes.nameStyle}>{name}</h1>
+                                            <h2 className={classes.companyNameStyle} >{image[0].companyName}</h2>
+                                            <img src ='assets/images/background/background.png' className={classes.backGround} alt={'port-0'}/>
+                                            <div className={classes.photo}>
+                                                <img src = {imgSrc} className={classes.photoImg} delay={100} alt={'port-1'}/>
+                                            </div>
+                                        </React.Fragment>
+                                    )}
                                     
                                 </div>
                             </Modal>
-                        </div>
+                        </React.Fragment>
                         )
                 }
                 innerScroll
