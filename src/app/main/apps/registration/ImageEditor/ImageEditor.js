3e7072d2932ed4964b67a0b898eeb04e72b40ccf
OFlag1014 <humblehorse1014@gmail.com>
2019-12-02 22:17:18 +0200

    test merge request

--- src/app/main/apps/registration/ImageEditor/ImageEditor.js
+++ src/app/main/apps/registration/ImageEditor/ImageEditor.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from "react";
 import "tui-image-editor/dist/tui-image-editor.css";
 import ImageEditor from "@toast-ui/react-image-editor";
 import {Button} from '@material-ui/core';
+
 const icona = require("tui-image-editor/dist/svg/icon-a.svg");
 const iconb = require("tui-image-editor/dist/svg/icon-b.svg");
 const iconc = require("tui-image-editor/dist/svg/icon-c.svg");
 const icond = require("tui-image-editor/dist/svg/icon-d.svg");
-const download = require("downloadjs");
+// const download = require("downloadjs");
+
 const myTheme = {
   "menu.backgroundColor": "white",
   "common.backgroundColor": "#151515",
@@ -24,37 +26,13 @@ function HomePage(props) {
     const [imageInst, setImageInst] = useState(null);
     const [image, setImage] = useState(props.image);
     const imageEditor = React.createRef();
-    
-    // const imageEditor = React.createRef();
-    
-    // console.log("ImageEditor",imageEditor);
-    // const imageEditorInst = imageEditor && imageEditor.current && imageEditor.current.imageEditorInst;
-    // const data = imageEditorInst && imageEditorInst.toDataURL();
-    // console.log("data",data)
 
     useEffect(() => {
       setImageInst(imageEditor.current.imageEditorInst || null);
     }, [imageEditor.current]);
 
-    console.log('here crop data: ', imageInst && imageInst.toDataURL());
-
-    // const saveImageToDisk = () => {
-    //   const imageEditorInst = imageEditor.current.imageEditorInst;
-    //   console.log("imageEditorInst",imageEditorInst)
-    //   const data = imageEditorInst.toDataURL();
-    //   console.log("data",data)
-    //   // if (data) {
-    //   //   const mimeType = data.split(";")[0];
-    //   //   const extension = data.split(";")[0].split("/")[1];
-    //   //   download(data, `image.${extension}`, mimeType);
-    //   // }
-    // };
-
     return (
-      <div className="home-page">
-        <div className="center">
-          {/* <Button className='button' onClick={saveImageToDisk}>Save Image to Disk</Button> */}
-        </div>
+      <React.Fragment>
         <ImageEditor
           includeUI={{
             loadImage: {
@@ -78,7 +56,7 @@ function HomePage(props) {
           usageStatistics={true}
           ref={imageEditor}
         />
-      </div>
+      </React.Fragment>
     );
   }
 
