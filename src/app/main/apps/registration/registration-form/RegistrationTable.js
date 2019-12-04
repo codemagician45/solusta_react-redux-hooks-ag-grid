1b60dcac6ac3a4b3cb8e31a1900b543c47ddf666
OFlag1014 <humblehorse1014@gmail.com>
2019-12-04 23:30:30 +0800

    Badge Mass print

--- src/app/main/apps/registration/registration-form/RegistrationTable.js
+++ src/app/main/apps/registration/registration-form/RegistrationTable.js
@@ -1,21 +1,68 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import { AllModules } from '@ag-grid-enterprise/all-modules';
+import ReactToPrint from 'react-to-print'; // for Print React component
+
+// import ag-grid css
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 import "@ag-grid-enterprise/all-modules/dist/styles/ag-grid.css";
 import "@ag-grid-enterprise/all-modules/dist/styles/ag-theme-balham.css";
 
+// import @material-ui components
+import { Box, Button } from '@material-ui/core';
+
+// import Redux
 import {withRouter} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
-
 import * as Actions from '../store/actions';
+
+// import components
 import ImageRender from './ImageRender';
 
+import BackgroundImg from '../assets/images/background.png';
+
+class PrintComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data : props.data,
+        };
+    }
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+        if (nextProps.data !== prevState.data) {
+            return {
+                data: nextProps.data,
+            }
+        } else {
+            return null;
+        }
+    }
+
+    render() {
+        const { data } = this.state;
+        return (
+            <div>
+                {data && data.map((item, index) => (
+                    <Box className="w-100 h-100" display="none" displayPrint="block" m={1} key={index.toString()}>
+                        <h1>{item.firstName + ' ' + item.lastName}</h1>
+                        <h2 >{item.companyName}</h2>
+                        <img src={BackgroundImg} alt="background"/>
+                        <div>
+                            <img src={`data:${item.mainPhotoContentType};base64, ${item.mainPhoto}`} alt="badge"/>
+                        </div>
+                    </Box>
+                ))}
+            </div>
+        );
+    }
+}
 
 function RegistrationTable(props) {
     const dispatch = useDispatch();
     const products = useSelector(({registerApp}) => registerApp.products.data);
+    const printRef = useRef();
     console.log('here in Registration table: ', products);
 
     useEffect(() => {
@@ -62,28 +109,39 @@ function RegistrationTable(props) {
     const headerHeight = () => {return 32;};
 
     return (
-        <div
-          className="table-responsive ag-theme-balham"
-          style={{height:'100%', width: '100%', fontSize: '16px' }}
-        >
-            <AgGridReact
-                columnDefs={columnDefs}
-                defaultColDef={defs.defaultColDef}
-                // rowModelType={'serverSide'}
-                rowData={rowData}
-                frameworkComponents = {frameworkComponents}
-                // onGridReady={onGridReady}
-                pagination={true}
-                getRowHeight = {getRowHeight}
-                headerHeight = {headerHeight}
-                floatingFilter = {true}
-                overlayLoadingTemplate={defs.overlayLoadingTemplate}
-                overlayNoRowsTemplate={defs.overlayNoRowsTemplate}
-                // modules={defs.modules}
-                // sideBar={defs.sideBar}
-                >
-            </AgGridReact>
-        </div>
+        <React.Fragment>
+            <div>
+                <ReactToPrint
+                    trigger={() => <Button color="secondary">Print Image</Button>}
+                    content={() => printRef.current}
+                />
+            </div>
+            <div
+                className="table-responsive ag-theme-balham"
+                style={{height:'100%', width: '100%', fontSize: '16px' }}
+            >
+                <AgGridReact
+                    columnDefs={columnDefs}
+                    defaultColDef={defs.defaultColDef}
+                    // rowModelType={'serverSide'}
+                    rowData={rowData}
+                    frameworkComponents = {frameworkComponents}
+                    // onGridReady={onGridReady}
+                    pagination={true}
+                    getRowHeight = {getRowHeight}
+                    headerHeight = {headerHeight}
+                    floatingFilter = {true}
+                    overlayLoadingTemplate={defs.overlayLoadingTemplate}
+                    overlayNoRowsTemplate={defs.overlayNoRowsTemplate}
+                    // modules={defs.modules}
+                    // sideBar={defs.sideBar}
+                    >
+                </AgGridReact>
+            </div>
+            <div className="w-100">
+                <PrintComponent data={products} ref={printRef}/>
+            </div>
+        </React.Fragment>
     );
 }
 
