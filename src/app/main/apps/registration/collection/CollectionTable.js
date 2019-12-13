0a5b6d2ec28d70a1d9b28ec57bd288769bdfc679
OFlag1014 <humblehorse1014@gmail.com>
2019-12-14 03:06:00 +0800

    Fix Security Approval page

--- src/app/main/apps/registration/collection/CollectionTable.js
+++ src/app/main/apps/registration/collection/CollectionTable.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 // import Ag-grid module
 import { AgGridReact } from 'ag-grid-react';
